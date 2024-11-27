4e7bccebfc35ccc90b5fd67d93156e0c95ead138
kalyan199999 <gottapupavankalyan@gmail.com>
2024-11-27 21:26:36 +0530

    mysql_node_js

--- web_development_2/backend/server.js
+++ web_development_2/backend/server.js
@@ -20,6 +20,9 @@ app.get("/getstudents" , (req,res)=>{
 
     const sql_query = "select * from samplestudenttable";
 
+    console.log("get method");
+    
+
     connection.query(sql_query , (err , result)=>{
         
         if(err)
@@ -31,11 +34,28 @@ app.get("/getstudents" , (req,res)=>{
     })
 })
 
+app.get("/getstudents/:id" , (req,res)=>{
+
+    const sql = "select * from samplestudenttable where id = ?";
+    const id = req.params.id;
+    // console.log(req.params);
+    console.log("get method:"+id);
+    
+    connection.query(sql , [id] , (err,data)=>{
+        if(err) return res.json({message:"Error fround"});
+
+        return res.json(data);
+    })
+
+})
+
+
 app.post("/addstudent" , (req,res)=>{
 
     const sql_query = "insert into samplestudenttable(`name`,`email`) values(?)";
 
     console.log(req.body);
+    console.log("post method");
 
     const values =[
         req.body.name,
@@ -53,8 +73,41 @@ app.post("/addstudent" , (req,res)=>{
         return res.json(data);
     })
     
+})
 
+app.put("/updatestudent/:id" , (req,res)=>{
 
+    const sql = 'UPDATE samplestudenttable SET name=? , email = ? WHERE id = ?';
+
+    const id = req.params.id;
+
+    console.log("put method"+id);
+    
+    connection.query(sql , [req.body.name , req.body.email , id] , (err,data)=>{
+
+        if(err) return res.json({message:"Error fround"});
+        
+        return res.json(data);
+    })
+})
+
+app.delete("/deletestudent/:id" , (req,res)=>{
+    const sql = "delete from samplestudenttable where id = ?";
+
+    const id = req.params.id;
+
+    console.log("delete:"+id);
+    
+
+    connection.query(sql , [id] , (err , result)=>{
+        console.log(err);
+        
+        if(err){
+            return res.json({message:"Error found"});
+        }
+
+        return res.json(result.affectedRows);
+    })
 })
 
 app.listen(PORT , ()=>{
